56ae70435bc6136e9f94a2e1d1192cc9875ab1b6
HelixFix <jerome.rinner@gmail.com>
2023-04-11 12:45:03 +0200

    Pointer change when hover replaced word

--- content_script.js
+++ content_script.js
@@ -1,28 +1,33 @@
+
+function addCursorPointer(event) {
+  event.target.style.cursor = "help";
+}
+
+function removeCursorPointer(event) {
+  event.target.style.cursor = "";
+}
+
+let counter = 0;
 // Mettre tous les mots à remplacer et leurs correspondances dans un objet
-const wordsToReplace = {
+let replacedWords = {
   "Plan social"     : "Plan de licenciement",
   "charges sociales": "cotisations sociales",
   "inflation"       : "hausse des prix",
   "taux d'intérêt"  : "coût du crédit"
 };
 
-let counter = 0;
-
 /**
- * Cette fonction remplace certains mots dans le corps HTML avec leurs correspondances.
+ * This function replaces certain words in the HTML body with their corresponding replacements.
  */
 function replaceWords() {
   let body = document.body.innerHTML;
-
-  for (const word in wordsToReplace) {
-    const regex = new RegExp(word, "gi");
-    body = body.replace(regex, function(match) {
+  for (let [key, value] of Object.entries(replacedWords)) {
+    let replacedBody = body.replace(new RegExp(key, "gi"), function(match) {
       counter++;
-      return `<span style="text-decoration: underline dotted;" title="${match}">${wordsToReplace[word]}</span>`;
+      return "<span style='text-decoration: underline dotted;' title='" + match + "'>" + value + "</span>";
     });
+    document.body.innerHTML = replacedBody;
   }
-
-  document.body.innerHTML = body;
   updateIcon(counter);
 }
 
@@ -39,3 +44,10 @@ if (!document.querySelector('.gdpr-lmd-wall__content')) {
     // La classe est présente, ne rien faire
     console.log('La classe est présente, le script est désactivé.');
   }
+
+// Ajoute les événements de survol pour les mots remplacés
+let replacedSpans = document.querySelectorAll("span[style='text-decoration: underline dotted;']");
+replacedSpans.forEach(function(replacedSpan) {
+  replacedSpan.addEventListener("mouseenter", addCursorPointer);
+  replacedSpan.addEventListener("mouseleave", removeCursorPointer);
+});
